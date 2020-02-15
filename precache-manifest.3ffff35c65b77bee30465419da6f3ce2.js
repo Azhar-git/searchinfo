fcdcb15c1775eb2fdd447accbd2408817a029bf8
Azhar-git <60664855+Azhar-git@users.noreply.github.com>
2020-02-15 15:13:20 +0530

    Updates

---
+++ precache-manifest.3ffff35c65b77bee30465419da6f3ce2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001195bb0a06dd2fbd28e892a5c02189",
+    "url": "/searchinfo/index.html"
+  },
+  {
+    "revision": "862142b701108992e573",
+    "url": "/searchinfo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8f9ff124103115f5f6bd",
+    "url": "/searchinfo/static/css/main.302c7d06.chunk.css"
+  },
+  {
+    "revision": "862142b701108992e573",
+    "url": "/searchinfo/static/js/2.4194dffd.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/searchinfo/static/js/2.4194dffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9ff124103115f5f6bd",
+    "url": "/searchinfo/static/js/main.c0455b7b.chunk.js"
+  },
+  {
+    "revision": "1bab86fb5f31ba07c09f",
+    "url": "/searchinfo/static/js/runtime-main.a72b5e93.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/searchinfo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
